5c4636e257dd9fe7cfdc6ed1c1db335138b94e61
werminskigabriel <gwerminski@student.agh.edu.pl>
2026-05-31 21:31:24 +0200

    init spring boot

---
+++ src/main/java/pl/GabrielW/catan_server/CatanServerApplication.java
@@ -0,0 +1,13 @@
+package pl.GabrielW.catan_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatanServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/GabrielW/catan_server/CatanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.GabrielW.catan_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
