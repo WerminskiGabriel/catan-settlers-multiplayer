e120eb351cb17873c1e703c1a97139196590c0a0
werminskigabriel <gwerminski@student.agh.edu.pl>
2026-06-06 19:38:45 +0200

    add Ocean type for outer hexes

---
+++ src/main/java/pl/GabrielW/catan_server/gameEngine/TurnPhase.java
@@ -0,0 +1,11 @@
+package pl.GabrielW.catan_server.gameEngine;
+
+public enum TurnPhase {
+    ROLL_PHASE,
+    TRADE_AND_BUILD,
+    DISCARD_PHASE,
+    ROBBER_PLACEMENT,
+    STEAL_PHASE,
+    GAME_OVER,
+    FIRST_2_TURNS
+}
